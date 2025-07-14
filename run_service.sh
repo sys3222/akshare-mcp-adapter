8f2ea8d5d3993074056461b80c89b9f932c18ba6
sys3222 <sys3222@msn.com>
2025-07-14 15:03:42 +0000

    Clean up unnecessary files

--- run_service.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run the service in the background
-cd "$(dirname "$0")"
-python main.py > service.log 2>&1 &
-echo $! > service.pid
-echo "Service started with PID $(cat service.pid)"
-echo "Logs are being written to service.log"
